36faee4f22e621c29f27fc8abd2c2b6341115224
fahimnirjon <fahimnirjon21@gmail.com>
2026-05-03 18:56:16 +0600

    Add Clerk authentication and webhook handling; update dependencies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,20 @@
 import express from "express";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+import { clerkWebhookHandler } from "./webhooks/clerk";
 
 const app = express();
 
-app.listen(3000, () => console.log("listening on port 3000"));
+const rawJson = express.raw({ type: 'application/json', limit: '1mb' });
+
+// it is important to use the rawJson middleware before the clerkMiddleware for the webhook route, otherwise the clerkMiddleware will consume the request body and the clerkWebhookHandler will not be able to verify the webhook signature
+
+app.post('/webhooks/clerk', rawJson, (req, res)=>{
+    void clerkWebhookHandler(req, res);
+})
+
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
+
+app.listen(3001, () => console.log("listening on port 3001"));
