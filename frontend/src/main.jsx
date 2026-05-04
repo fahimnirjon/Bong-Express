1e5d409bec0ef37333438edb710792c75e5da612
fahimnirjon <fahimnirjon21@gmail.com>
2026-05-05 01:29:49 +0600

    Refactor import statements and formatting in main.jsx for consistency

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ClerkProvider } from '@clerk/react'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ClerkProvider } from "@clerk/react";
 
+const queryClient = new QueryClient();
 
-const queryClient = new QueryClient()
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider>
       <QueryClientProvider client={queryClient}>
@@ -16,4 +15,4 @@ createRoot(document.getElementById('root')).render(
       </QueryClientProvider>
     </ClerkProvider>
   </StrictMode>,
-)
+);
