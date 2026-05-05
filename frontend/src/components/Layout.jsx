162103afc9f63c6b2e88efe3bb308c0154d8b89b
fahimnirjon <fahimnirjon21@gmail.com>
2026-05-05 13:35:40 +0600

    Add Zustand for state management; implement useCart for cart functionality with persistence

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,10 +1,10 @@
 // import Footer from "./Footer";
-// import Navbar from "./Navbar";
+import Navbar from "./Navbar";
 
 function Layout({ children }) {
   return (
     <div className="flex min-h-svh flex-col bg-base-200 text-base-content">
-      {/* <Navbar /> */}
+      <Navbar />
 
       <main className="mx-auto w-full max-w-7xl flex-1 px-4 py-8 md:px-6 md:py-10">{children}</main>
 
